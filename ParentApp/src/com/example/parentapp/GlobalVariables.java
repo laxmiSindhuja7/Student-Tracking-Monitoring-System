46b0b582508197a641dbdc4dd7c38dd54609008b
laxmiSindhuja7 <laxmisindhuja555@example.com>
2024-05-27 19:30:58 -0400

    project overview

--- ParentApp/src/com/example/parentapp/GlobalVariables.java
+++
@@ -1,9 +0,0 @@
-package com.example.parentapp;
-
-public class GlobalVariables
-{
-	public static String userid="",usertype="",phoneno="",busno="",sid="";
-	public static String titlebarcolor="#191970";
-	
-
-}
